f78da5dddd1d2b9eb0b855306d062b8ecbc12199
Cifula <lidel95@gmail.com>
2023-02-15 09:12:32 +0900

    no message

---
+++ src/simplechatting2/dto/MessageRespDto.java
@@ -0,0 +1,12 @@
+package simplechatting2.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@AllArgsConstructor
+@Data
+public class MessageRespDto {
+	private String messageValue;
+
+}
